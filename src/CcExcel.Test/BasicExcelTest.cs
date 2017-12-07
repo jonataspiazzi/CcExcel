46adfc738d4f094c41aa44964418d89acdc9af7d
Jonatas Piazzi <piazzi_7@hotmail.com>
2017-12-07 18:46:30 -0200

    Fix in unit tests.

--- src/CcExcel.Test/BasicExcelTest.cs
+++ src/CcExcel.Test/BasicExcelTest.cs
@@ -17,23 +17,23 @@ namespace CcExcel.Test
             using (var rs = GetType().Assembly.GetManifestResourceStream("CcExcel.Test.Resources.AllTypes.xlsx"))
             using (var excel = new Excel(rs, ExcelMode.OpenReadOnly))
             {
-                Assert.AreEqual("general", (string)excel["Sheet1"].Values["B", 2]);
-                Assert.AreEqual("12.4568", (string)excel["Sheet1"].Values["B", 3]);
-                Assert.AreEqual("45.25", (string)excel["Sheet1"].Values["B", 4]);
-                Assert.AreEqual("18.56", (string)excel["Sheet1"].Values["B", 5]);
-                Assert.AreEqual("32408", (string)excel["Sheet1"].Values["B", 6]);
-                Assert.AreEqual("42952", (string)excel["Sheet1"].Values["B", 7]);
-                Assert.AreEqual("0.49", (string)excel["Sheet1"].Values["B", 8]);
-                Assert.AreEqual("0.1845", (string)excel["Sheet1"].Values["B", 9]);
-                Assert.AreEqual("0.2", (string)excel["Sheet1"].Values["B", 10]);
-                Assert.AreEqual("10500000", (string)excel["Sheet1"].Values["B", 11]);
-                Assert.AreEqual("text1", (string)excel["Sheet1"].Values["B", 12]);
-                Assert.AreEqual("text2", (string)excel["Sheet1"].Values["B", 13]);
-                Assert.AreEqual("text1", (string)excel["Sheet1"].Values["B", 14]);
-                Assert.AreEqual("text2", (string)excel["Sheet1"].Values["B", 15]);
-                Assert.AreEqual("a", (string)excel["Sheet1"].Values["B", 16]);
-                Assert.AreEqual("1", (string)excel["Sheet1"].Values["B", 17]);
-                Assert.IsNull((string)excel["Sheet1"].Values["B", 18]);
+                Assert.AreEqual("general", excel["Sheet1"].Values["B", 2].ToString());
+                Assert.AreEqual("12.4568", excel["Sheet1"].Values["B", 3].ToString());
+                Assert.AreEqual("45.25", excel["Sheet1"].Values["B", 4].ToString());
+                Assert.AreEqual("18.56", excel["Sheet1"].Values["B", 5].ToString());
+                Assert.AreEqual("32408", excel["Sheet1"].Values["B", 6].ToString());
+                Assert.AreEqual("42952", excel["Sheet1"].Values["B", 7].ToString());
+                Assert.AreEqual("0.49", excel["Sheet1"].Values["B", 8].ToString());
+                Assert.AreEqual("0.1845", excel["Sheet1"].Values["B", 9].ToString());
+                Assert.AreEqual("0.2", excel["Sheet1"].Values["B", 10].ToString());
+                Assert.AreEqual("10500000", excel["Sheet1"].Values["B", 11].ToString());
+                Assert.AreEqual("text1", excel["Sheet1"].Values["B", 12].ToString());
+                Assert.AreEqual("text2", excel["Sheet1"].Values["B", 13].ToString());
+                Assert.AreEqual("text1", excel["Sheet1"].Values["B", 14].ToString());
+                Assert.AreEqual("text2", excel["Sheet1"].Values["B", 15].ToString());
+                Assert.AreEqual("a", excel["Sheet1"].Values["B", 16].ToString());
+                Assert.AreEqual("1", excel["Sheet1"].Values["B", 17].ToString());
+                Assert.IsNull(excel["Sheet1"].Values["B", 18].ToString());
             }
         }
 
@@ -43,23 +43,29 @@ namespace CcExcel.Test
             using (var rs = GetType().Assembly.GetManifestResourceStream("CcExcel.Test.Resources.AllTypes.xlsx"))
             using (var excel = new Excel(rs, ExcelMode.OpenReadOnly))
             {
-                Assert.AreEqual("general", excel["Sheet1"].Values["B", 2]);
-                Assert.AreEqual(12.4568, excel["Sheet1"].Values["B", 3]);
-                Assert.AreEqual(45.25M, excel["Sheet1"].Values["B", 4]);
-                Assert.AreEqual(18.56, excel["Sheet1"].Values["B", 5]);
-                Assert.AreEqual(new DateTime(1988, 22, 09), excel["Sheet1"].Values["B", 6]);
-                Assert.AreEqual(new DateTime(2017, 8, 5), excel["Sheet1"].Values["B", 7]);
-                Assert.AreEqual(new TimeSpan(11, 45, 36), excel["Sheet1"].Values["B", 8]);
-                Assert.AreEqual((float?)0.1845, excel["Sheet1"].Values["B", 9]);
-                Assert.AreEqual((double?)0.2, excel["Sheet1"].Values["B", 10]);
-                Assert.AreEqual(10500000, excel["Sheet1"].Values["B", 11]);
-                Assert.AreEqual("text1", excel["Sheet1"].Values["B", 12]);
-                Assert.AreEqual("text2", excel["Sheet1"].Values["B", 13]);
-                Assert.AreEqual("text1", excel["Sheet1"].Values["B", 14]);
-                Assert.AreEqual("text2", excel["Sheet1"].Values["B", 15]);
-                Assert.AreEqual('a', excel["Sheet1"].Values["B", 16]);
-                Assert.AreEqual(true, excel["Sheet1"].Values["B", 17]);
-                Assert.IsNull(excel["Sheet1"].Values["B", 18]);
+                Assert.AreEqual("general", excel["Sheet1"].Values["B", 2].ToString());
+                Assert.AreEqual(12.4568, excel["Sheet1"].Values["B", 3].ToDouble());
+                Assert.AreEqual(45.25M, excel["Sheet1"].Values["B", 4].ToDecimal());
+                Assert.AreEqual(18.56f, excel["Sheet1"].Values["B", 5].ToSingle());
+                Assert.AreEqual(new DateTime(1988, 9, 22), excel["Sheet1"].Values["B", 6].ToDateTime());
+                Assert.AreEqual(new DateTime(2017, 8, 5), excel["Sheet1"].Values["B", 7].ToDateTime());
+                Assert.AreEqual(new TimeSpan(11, 45, 36), excel["Sheet1"].Values["B", 8].ToTimeSpan());
+                Assert.AreEqual((float?)0.1845, excel["Sheet1"].Values["B", 9].ToNullableSingle());
+                Assert.AreEqual((double?)0.2, excel["Sheet1"].Values["B", 10].ToNullableDouble());
+                Assert.AreEqual(10500000, excel["Sheet1"].Values["B", 11].ToInt32());
+                Assert.AreEqual(10500000L, excel["Sheet1"].Values["B", 11].ToInt64());
+                Assert.AreEqual("text1", excel["Sheet1"].Values["B", 12].ToString());
+                Assert.AreEqual("text2", excel["Sheet1"].Values["B", 13].ToString());
+                Assert.AreEqual("text1", excel["Sheet1"].Values["B", 14].ToString());
+                Assert.AreEqual("text2", excel["Sheet1"].Values["B", 15].ToString());
+                Assert.AreEqual('a', excel["Sheet1"].Values["B", 16].ToChar());
+                Assert.AreEqual(true, excel["Sheet1"].Values["B", 17].ToBoolean());
+                Assert.IsNull(excel["Sheet1"].Values["B", 18].ToNullableBoolean());
+                Assert.IsNull(excel["Sheet1"].Values["B", 18].ToNullableInt32());
+                Assert.IsNull(excel["Sheet1"].Values["B", 18].ToNullableDouble());
+                Assert.IsNull(excel["Sheet1"].Values["B", 18].ToNullableDecimal());
+                Assert.IsNull(excel["Sheet1"].Values["B", 18].ToNullableDateTime());
+                Assert.IsNull(excel["Sheet1"].Values["B", 18].ToNullableTimeSpan());
             }
         }
 
@@ -79,7 +85,7 @@ namespace CcExcel.Test
 
                 using (var excel = new Excel(ms, ExcelMode.OpenReadOnly))
                 {
-                    string value = excel["test"].Values["b", 2];
+                    var value = excel["test"].Values["b", 2].ToString();
 
                     Assert.AreEqual("info", value);
                 }
